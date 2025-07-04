3f5d687f8647238229be1ce877a08105e5610ea7
Prasanth-S7 <prasanthsampath2005@gmail.com>
2025-07-04 20:45:16 +0530

    feat: update the schema.sql to store the refresh token

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     badge_received INTEGER DEFAULT 0,
     badgr_username TEXT,
     encrypted_bearer_token TEXT,  
+    encrypted_refresh_token TEXT,
     token_expires_at DATETIME,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
