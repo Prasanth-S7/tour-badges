7487b75565fdfbdcb0e1c6a92bdb25768a55d8ac
Prasanth-S7 <prasanthsampath2005@gmail.com>
2025-07-20 01:02:59 +0530

    chore: deleted validation.ts since we use oAuth

--- src/utils/validation.ts
+++
@@ -1,8 +0,0 @@
-export const isValidEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
-
-export const isValidName = (name: string) => {
-    return name.trim().length >=2 && name.trim().length <=100
-}
